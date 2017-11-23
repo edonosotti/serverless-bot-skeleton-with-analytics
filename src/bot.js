cdaf9c2db1fe64da607d2384e92637501a4d7d83
Edoardo Nosotti <edoardo.nosotti@gmail.com>
2017-11-23 21:18:01 +0100

    Update main module.

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const botBuilder = require('claudia-bot-builder');
+const BotBuilder = require('claudia-bot-builder');
+const MessageProcessor = require('./lib/messageprocessor');
 
 // See: https://github.com/claudiajs/claudia-bot-builder/blob/c76ac0a2234d1ce51c4e6c566c076207a707d759/docs/API.md#selecting-platforms
 // to learn about platforms supported by claudia-bot-builder.
@@ -10,10 +11,17 @@ var enabledPlatforms = [
   'skype',
   'viber',
   'slackSlashCommand',
-  'alexa',
-  'line'
+  'alexa'
 ];
 
-module.exports = botBuilder(function(message) {
-  return message.text;
+// Create an instance of Claudia Bot Builder
+module.exports = BotBuilder(function(message, request) {
+  // Create an instance of the custom Message Processor
+  var messageProcessor = new MessageProcessor(
+    request,
+    process.env.DIALOGFLOW_CLIENT_ACCESS_KEY,
+    process.env.CHATBASE_API_KEY
+  );
+  // Return a response to the user
+  return messageProcessor.processMessage(message);
 }, { platforms: enabledPlatforms });
