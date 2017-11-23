72ee2df00073d318138efee14fb59d42aeda3b35
Edoardo Nosotti <edoardo.nosotti@gmail.com>
2017-11-23 20:45:29 +0100

    Add string resources.

--- src/res/en/strings.js
+++ src/res/en/strings.js
@@ -1,6 +1,19 @@
+'use strict';
+
 module.exports = {
   SPEC_TEST_STRING: 'SPEC_TEST_STRING', // DO NOT TRANSLATE!
   SPEC_TEST_TEMPLATE_STRING: 'SPEC_TEST_{{TEMPLATE}}_STRING', // DO NOT TRANSLATE!
+
+  BUTTON_OPEN_IMAGE: 'Open',
+  LABEL_LIST: 'Available images:',
+  LABEL_AVAILABLE_OPTIONS: 'Available options:',
+  LABEL_IMAGE: 'Image #{{ID}}',
+  LABEL_BY: 'by {{AUTHOR}}',
+
+  SLACK_ATTACHMENT_ERROR: 'This attachment is not supported in your version of Slack',
+
+  ALEXA_IMAGES_TITLE: 'I have {{COUNT}} images in archive:',
+  ALEXA_GOODBYE: 'Goodbye!',
 
   DID_NOT_UNDERSTAND: [
     'I am sorry, I could not understand your message. I am still a young bot ;) May I ask you to rephrase and try again?',
