11bfe947f55ed755d1c2322c5da9f118181a1c40
Edoardo Nosotti <edoardo.nosotti@gmail.com>
2017-11-23 20:45:46 +0100

    Add intents and demo data.

---
+++ src/lib/data.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+  // A fake dataset for demo purposes
+  IMAGES: [
+    { ID: 1074, AUTHOR: 'Samuel Scrimshaw', URL: 'https://unsplash.com/photos/sseiVD2XsOk' },
+    { ID: 1039, AUTHOR: 'Andrew Coelho', URL: 'https://unsplash.com/photos/VB-w_3dnyvI' },
+    { ID: 973, AUTHOR: 'Cameron Kirby', URL: 'https://unsplash.com/photos/n3t4fIuVzLA' },
+  ],
+}
---
+++ src/lib/intents.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+  // Values *MUST* match the "action" parameters
+  // set in Dialogflow for each available Intent
+  UNKNOWN: 'input.unknown',
+  WELCOME: 'input.welcome',
+  MAGIC_TRICKS: 'input.magic_tricks',
+}
